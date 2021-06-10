337f7e47992615ea0707bc787d722a21f2d748b1
Kaz Gofron <kgofron@bnl.gov>
2021-06-10 14:12:07 -0400

    [IXS] ReIP and Debian 10 migration

--- iocBoot/ioccc/st.cmd
+++ iocBoot/ioccc/st.cmd
@@ -1,6 +1,7 @@
 #!../../bin/linux-x86_64/cc
 
 < envPaths
+< /epics/common/xf10idd-ioc3-netsetup.cmd
 
 epicsEnvSet("ENGINEER",  "kgofron x5283")
 epicsEnvSet("LOCATION",  "740 IXS RG:B1")
@@ -8,8 +9,6 @@ epicsEnvSet("LOCATION",  "740 IXS RG:B1")
 epicsEnvSet("Sys",  "XF:10IDA-UT")
 epicsEnvSet("Dev",  "Cryo:1")
 
-epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
-epicsEnvSet("EPICS_CA_ADDR_LIST", "10.10.0.255")
 epicsEnvSet("STREAM_PROTOCOL_PATH", "ccApp/Db")
 
 cd ${TOP}
@@ -18,8 +17,11 @@ cd ${TOP}
 dbLoadDatabase("dbd/cc.dbd")
 cc_registerRecordDeviceDriver(pdbbase)
 
-drvAsynIPPortConfigure("CC","10.10.2.43:2000")
+drvAsynIPPortConfigure("CC","xf10id-cryo1.nsls2.bnl.local:2000")
 dbLoadTemplate("db/cc.substitutions", "SYS=XF:10IDA-UT,PDEV=Cryo:1,PORT=CC")
+
+dbLoadRecords("$(EPICS_BASE)/db/iocAdminSoft.db","IOC=XF:10IDA-CT{IOC-Cryo1}")
+dbLoadRecords("db/reccaster.db", "P=XF:10IDA-CT{IOC-Cryo1}RecSync")
 
 #asynSetTraceMask CC , -1, 0x9
 #asynSetTraceIOMask CC, -1, 0x2
